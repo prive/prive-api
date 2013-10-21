c7e924af982ad7c5481345819cd2016a6a66974b
Paul Golubev <pavel@golubeff.ru>
2013-10-21 16:45:48 -0700

    debug

--- config/database.rb
+++ config/database.rb
@@ -1,4 +1,5 @@
 require Padrino.root('config/sensitive_settings.rb')
 Sequel::Model.plugin(:schema)
 Sequel::Model.raise_on_save_failure = false # Do not throw exceptions on failure
+puts "Padrino: #{Padrino.env}"
 Sequel::Model.db = Sequel.connect(SensitiveSettings.database(Padrino.env), :loggers => [logger])
